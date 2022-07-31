91234e5c7bc69652e98658a699a9f20510106f1c
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-31 12:44:42 +0300

    Login page css complated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import Navbar from "../components/Navbar";
+import Footer from '../components/Footer';
+import styled from 'styled-components'
+
+const Container = styled.div`
+width:100vw;
+height:80vh;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color:whitesmoke;
+`
+const Wrapper = styled.div`
+width:60%;
+height:100%;        
+padding:20 px; 
+align-items: center;
+justify-content: center;
+background-color:#EEEEEE;
+`
+const HeroTitle = styled.h1`
+font-size:32px;
+font-weight:500;
+text-align:center;
+padding-top:100px;
+padding-bottom:20px;
+border-bottom: 1px solid rgba(0,0,0,.25);
+`
+
+const Form = styled.form`
+display: flex;
+flex-wrap: wrap;
+`
+const Input = styled.input`
+flex:1;
+margin:20px 10px 0px 0px;
+padding:8px;
+`
+const Button = styled.button`
+min-width:40%;
+border:none;
+border-radius:15px;
+margin:20px 10px 0px 0px;
+padding:5px;
+background-color:teal;
+color:white;
+cursor:pointer;
+`
+const FormWrapper = styled.div`
+width:40%;
+margin:auto;
+display: flex;
+align-items: center;
+justify-content:center;
+flex-wrap: wrap;
+boder:1px solid black
+`
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <Navbar />
+      <Container>
+        <Wrapper>
+          <HeroTitle>Login</HeroTitle>
+          <FormWrapper>
+            <Form>
+              <Input placeholder="Email" required/>
+              <Input placeholder="Password" required/>
+              <Button>Login</Button>
+            </Form>
+          </FormWrapper>
+        </Wrapper>
+      </Container>
+      <Footer />
+    </>
   )
 }
 
