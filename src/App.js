17c90b1328110c3ece95f6917fa6f7d15539ff33
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-31 18:12:40 +0300

    LoginNavbar added and complated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import { Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import LoginNavbar from "./components/LoginNavbar";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/deneme" element={<LoginNavbar />} />
       </Routes>
     </div>
   );
---
+++ src/components/LoginNavbar.jsx
@@ -0,0 +1,103 @@
+import styled  from 'styled-components'
+import { Link } from "react-router-dom";
+import { Help, Logout } from '@mui/icons-material';
+
+const Container = styled.div`
+height:60px;
+background-color:#393E46;
+`
+
+const Wrapper = styled.div`
+padding:0px 20px;
+display:flex;
+align-items:center;
+justify-content:space-between
+`
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`
+
+const Logo = styled.div`
+font-family: 'Square Peg', cursive;
+font-size:32px;
+font-weight:600;
+cursor:pointer;
+color:#00ADB5;
+margin-left:10%;
+padding:10px 12px;
+`
+
+const MenuItem = styled.button`
+font-size: 14px;
+cursor: pointer;
+background-color: #393E46;
+color:#EEEEEE;
+padding:20px 12px;
+border:none;
+&:hover {
+    background-color: #222831;
+  }
+`
+
+const Right = styled.div`
+flex:1;
+display:flex;
+align-items: center;
+justify-content:flex-end;
+`
+
+const MenuItemFirst = styled.div`
+width:80px;
+margin-right:10%;
+margin-left:25px;
+cursor: pointer;
+color:#B5B5B6;
+padding:10px 20px;
+&:hover {
+    color: #EEEEEE;
+  }
+`
+
+const MenuItemSecond = styled.div`
+margin-left:25px;
+cursor: pointer;
+border:none;
+background-color:#0275d8;
+color:#EEEEEE;
+padding:10px 20px;
+border-radius:5px;
+&:hover {
+    background-color: #228CE9;
+  }
+`
+
+
+const LoginNavbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Logo>OEM</Logo>{/*online exam maker*/}
+                    <Link to="/deneme">
+                        <MenuItem>Dashboard</MenuItem>
+                    </Link>
+                    <MenuItem>Users</MenuItem>
+                    <MenuItem>Reports</MenuItem>
+                </Left>
+                <Right>
+                    <Link to="/login" style={{ textDecoration: "none" }}>
+                        <MenuItemSecond><Help style={{verticalAlign:"middle"}}/></MenuItemSecond>
+                    </Link>
+                    <Link to="/register" style={{ textDecoration: "none", marginRight: "10%" }}>
+                        <MenuItemFirst><Logout style={{verticalAlign:"middle"}}/> Logout</MenuItemFirst>
+                    </Link>
+                </Right>
+            </Wrapper>
+        </Container>
+  )
+}
+
+export default LoginNavbar
