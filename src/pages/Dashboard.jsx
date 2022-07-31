bf241e30e74564a01d65d76b3f15e2c22f7336a5
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-31 21:25:16 +0300

    Dashboard table updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,21 +8,23 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { BarChart, Delete, Edit, Visibility } from "@mui/icons-material";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Deneme sınavı 1', 159, 6.0, 24, 4.0),
+  createData('Deneme sınavı 2', 237, 9.0, 37, 4.3),
+  createData('Deneme sınavı 3', 262, 16.0, 24, 6.0),
+  createData('Deneme sınavı 4', 305, 3.7, 67, 4.3),
+  createData('Deneme sınavı 5', 356, 16.0, 49, 3.9),
 ];
 
 const Container = styled.table`
     width: 100%;
+    height:40vh;
     border-collapse: collapse;
     text-align: center;
     border-radius:8px;
@@ -34,7 +36,6 @@ const Dashboard = () => {
     <>
       <LoginNavbar />
       <Container>
-
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -55,10 +56,10 @@ const Dashboard = () => {
                   <TableCell component="th" scope="row">
                     {row.name}
                   </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
+                  <TableCell align="right"><button><BarChart/>Analyze</button></TableCell>
+                  <TableCell align="right"><button><Visibility/>Preview</button></TableCell>
+                  <TableCell align="right"><button><Edit/>Edit</button></TableCell>
+                  <TableCell align="right"><button><Delete/>Delete</button></TableCell>
                 </TableRow>
               ))}
             </TableBody>
